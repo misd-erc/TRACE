59505c5bac66ffb63f7687f1460d3fdee7dd3af1
Aldrin Cadiente <139181982+Aldrin1417@users.noreply.github.com>
2025-03-04 13:35:36 +0800

    notification dropdown
    notification dropdown

--- TRACE/wwwroot/js/site.js
+++ TRACE/wwwroot/js/site.js
@@ -3,8 +3,14 @@
         document.getElementById("loader").style.display = "none";
     }, 2000);
 });
-
+const _notifmenu = document.getElementById('notifDropdown');
+const _profilemenu = document.getElementById('profileDropdown');
 function toggleProfileMenu() {
-    const menu = document.getElementById('profileDropdown');
-    menu.classList.toggle('show');
+    _notifmenu.classList.remove('show');
+    _profilemenu.classList.toggle('show');
+}
+
+function toggleNotifMenu() {
+    _profilemenu.classList.remove('show')
+    _notifmenu.classList.toggle('show');
 }
