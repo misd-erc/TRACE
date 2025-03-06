c1a2792f5971293e6abee99c1697f95ee546ff9f
Aldrin Cadiente <139181982+Aldrin1417@users.noreply.github.com>
2025-03-06 17:29:23 +0800

    Settings update
    Settings update

--- TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
+++ TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90f27bbbd1e44306f9beae3db123a4d0b5687dbd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+426f867db61392ad459b7570757248f50dbb0b42")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
