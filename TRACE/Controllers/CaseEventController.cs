1235ff560f3bc36413f75b7877a198eda1640ed8
Jeremiah Barzaga <40589307+jeremiahbarzaga@users.noreply.github.com>
2025-03-27 11:51:11 +0800

    caseevent

---
+++ TRACE/Controllers/CaseEventController.cs
@@ -0,0 +1,181 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TRACE.Context;
+using TRACE.Models;
+
+namespace TRACE.Controllers
+{
+    public class CaseEventController : Controller
+    {
+        private readonly ErcdbContext _context;
+
+        public CaseEventController(ErcdbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: CaseEvent
+        public async Task<IActionResult> Index()
+        {
+            var ercdbContext = _context.CaseEvents.Include(c => c.CaseEventType).Include(c => c.Erccase);
+            return View(await ercdbContext.ToListAsync());
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCaseEventByErcID(int id)
+        {
+            var categories = await _context.CaseEvents.Where(x=>x.ErccaseId == id).ToListAsync();
+
+            if (categories == null || !categories.Any())
+            {
+                return Json(new { success = false, message = "No categories found." });
+            }
+
+            return Json(new { success = true, data = categories });
+        }
+        // GET: CaseEvent/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseEvent = await _context.CaseEvents
+                .Include(c => c.CaseEventType)
+                .Include(c => c.Erccase)
+                .FirstOrDefaultAsync(m => m.CaseEventId == id);
+            if (caseEvent == null)
+            {
+                return NotFound();
+            }
+
+            return View(caseEvent);
+        }
+
+        // GET: CaseEvent/Create
+        public IActionResult Create()
+        {
+            ViewData["CaseEventTypeId"] = new SelectList(_context.CaseEventTypes, "CaseEventTypeId", "CaseEventTypeId");
+            ViewData["ErccaseId"] = new SelectList(_context.Erccases, "ErccaseId", "ErccaseId");
+            return View();
+        }
+
+        // POST: CaseEvent/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("CaseEventId,ErccaseId,EventDatetime,UserId,EventDescription,IsUserAction,CaseEventTypeId")] CaseEvent caseEvent)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(caseEvent);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CaseEventTypeId"] = new SelectList(_context.CaseEventTypes, "CaseEventTypeId", "CaseEventTypeId", caseEvent.CaseEventTypeId);
+            ViewData["ErccaseId"] = new SelectList(_context.Erccases, "ErccaseId", "ErccaseId", caseEvent.ErccaseId);
+            return View(caseEvent);
+        }
+
+        // GET: CaseEvent/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseEvent = await _context.CaseEvents.FindAsync(id);
+            if (caseEvent == null)
+            {
+                return NotFound();
+            }
+            ViewData["CaseEventTypeId"] = new SelectList(_context.CaseEventTypes, "CaseEventTypeId", "CaseEventTypeId", caseEvent.CaseEventTypeId);
+            ViewData["ErccaseId"] = new SelectList(_context.Erccases, "ErccaseId", "ErccaseId", caseEvent.ErccaseId);
+            return View(caseEvent);
+        }
+
+        // POST: CaseEvent/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("CaseEventId,ErccaseId,EventDatetime,UserId,EventDescription,IsUserAction,CaseEventTypeId")] CaseEvent caseEvent)
+        {
+            if (id != caseEvent.CaseEventId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(caseEvent);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CaseEventExists(caseEvent.CaseEventId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CaseEventTypeId"] = new SelectList(_context.CaseEventTypes, "CaseEventTypeId", "CaseEventTypeId", caseEvent.CaseEventTypeId);
+            ViewData["ErccaseId"] = new SelectList(_context.Erccases, "ErccaseId", "ErccaseId", caseEvent.ErccaseId);
+            return View(caseEvent);
+        }
+
+        // GET: CaseEvent/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var caseEvent = await _context.CaseEvents
+                .Include(c => c.CaseEventType)
+                .Include(c => c.Erccase)
+                .FirstOrDefaultAsync(m => m.CaseEventId == id);
+            if (caseEvent == null)
+            {
+                return NotFound();
+            }
+
+            return View(caseEvent);
+        }
+
+        // POST: CaseEvent/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var caseEvent = await _context.CaseEvents.FindAsync(id);
+            if (caseEvent != null)
+            {
+                _context.CaseEvents.Remove(caseEvent);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool CaseEventExists(long id)
+        {
+            return _context.CaseEvents.Any(e => e.CaseEventId == id);
+        }
+    }
+}
