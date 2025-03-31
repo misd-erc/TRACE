6702d4fa2d7bb3a22a8364baa7f6876fd0245460
Aldrin Cadiente <139181982+Aldrin1417@users.noreply.github.com>
2025-03-31 14:42:18 +0800

    chnged default user creation role and designation

--- TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
+++ TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a16cb8e71e5429c12330702517a6d8634a012ccb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0140344b9d3f41713736f9602683798f323a2ef0")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
