727cbfe39eb3a5446a7c6c91b5d8f92cd0bd1400
Aldrin Cadiente <aldrinmaenardcadiente@gmail.com>
2025-05-15 10:54:19 +0800

    Added bold font weight for unread notifications
    Added bold font weight for unread notifications

--- TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
+++ TRACE/obj/Debug/net8.0/TRACE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b92e25f767673e110a925cf2677f3d51e94730a7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2ac6ae2b886202b9696d4175c75c32f83ec6f33")]
 [assembly: System.Reflection.AssemblyProductAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TRACE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
